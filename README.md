7e479f735257dfdd0ccc05885238cdb91c257b49
jesse.fuller <jesse.fuller@jamf.com>
2023-01-18 19:20:06 +0900

    Resetting back to base Sensirion version since there's some issue

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Code needed to run a CO2 sensor with an Arduino Uno and a Sensirion SCD41.
 I didn't want to pay 41,000 yen to buy a CO2 monitor or pay 7000 yen only to find out it doesn't use a real CO2 sensor.
 
 Here's my BOM:
-- Sensilion CO2 Sensor Module SCD41 - https://www.amazon.co.jp/gp/product/B097P1FN4Z/
-- Arduino Uno - https://amzn.asia/d/1yQqwCN
-
-I'll be adding a screen and probably a battery at some point.
+- Sensilion CO2 Sensor Module SCD41 - https://amzn.asia/d/65hkPqi
+- ESP32 Dev Board - https://amzn.asia/d/a0Z7G5y
+- Takachi Plastic Electronics Case - https://amzn.asia/d/7XtJIo1
+- Micro USB Extension Cable - https://amzn.asia/d/gPwMIfM
+- uxcell Header Strip - https://amzn.asia/d/1Z3lkxU
+- Penglin 5v Laptop Fans - https://amzn.asia/d/334x9Sz
+- Sanhayato SAD-101 Bread Board - https://amzn.asia/d/3W0BudY
+- Sanhayato SKS-100 Jump Wire Kit - https://amzn.asia/d/dvoNemy
+- 10Gtek RGB LED Module - https://amzn.asia/d/2ihQCmO
+- Rocker Switch - https://amzn.asia/d/a37Wsy4
--- co2-sensor.ino
+++ co2-sensor.ino
@@ -1,29 +1,14 @@
 // This code is based on Sensirion's Arduino Snippets
 // Check https://github.com/Sensirion/arduino-snippets for the most recent version.
 
-/*
-  This code is based on Rui Santos Snippets
-  Complete project details at https://RandomNerdTutorials.com/esp32-date-time-ntp-client-server-arduino/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-#include "esp_timer.h"
-#include "Sensirion_GadgetBle_Lib.h"
+#include "Sensirion_Gadget_BLE.h"
 #include <SensirionI2CScd4x.h>
-#include <Wire.h>
-#include <WiFi.h>
-#include "time.h"
-
 
 // GadgetBle workflow
-static int64_t lastMmntTime = 0;
-static int mmntIntervalUs = 5000000;
-GadgetBle gadgetBle = GadgetBle(GadgetBle::DataType::T_RH_CO2);
+static int64_t lastMeasurementTimeMs = 0;
+static int measurementIntervalMs = 5000;
+NimBLELibraryWrapper lib;
+DataProvider provider(lib, DataType::T_RH_CO2);
 
 SensirionI2CScd4x scd4x;
 
@@ -33,11 +18,6 @@ SensirionI2CScd4x scd4x;
 // const char* password = "mywifipassword";
 #include "config.h"
 
-// ntp setup
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
-
 void setup() {
   Serial.begin(115200);
   // wait for serial connection from PC
@@ -46,29 +26,10 @@ void setup() {
   while(!Serial);
 
   // Initialize the GadgetBle Library
-  gadgetBle.begin();
+  provider.begin();
   Serial.print("Sensirion GadgetBle Lib initialized with deviceId = ");
-  Serial.println(F(gadgetBle.getDeviceIdString()));
+  Serial.println(provider.getDeviceIdString());
   
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println((ssid));
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(F(""));
-  Serial.println(F("WiFi connected."));
-
-  // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
-
   // init I2C
   Wire.begin();
 
@@ -85,22 +46,20 @@ void setup() {
   if (error) {
       Serial.print("Error trying to execute startPeriodicMeasurement(): ");
       errorToString(error, errorMessage, 256);
-      Serial.println(F(errorMessage));
+      Serial.println(errorMessage);
   }
 
-  Serial.println(F("Waiting for first measurement... (5 sec)"));
-  Serial.println(F("CO2(ppm)\tTemperature(degC)\tRelativeHumidity(percent)"));
+  Serial.println("Waiting for first measurement... (5 sec)");
+  Serial.println("CO2(ppm)\tTemperature(degC)\tRelativeHumidity(percent)");
   delay(5000);
 }
 
 void loop() {
-  if (esp_timer_get_time() - lastMmntTime >= mmntIntervalUs) {
+  if (millis() - lastMeasurementTimeMs >= measurementIntervalMs) {
     measure_and_report();
   }
 
-  printLocalTime();
-
-  gadgetBle.handleEvents();
+  provider.handleDownload();
   delay(3);
 }
 
@@ -114,62 +73,33 @@ void measure_and_report() {
   float humidity;
 
   error = scd4x.readMeasurement(co2, temperature, humidity);
-  lastMmntTime = esp_timer_get_time();
+  lastMeasurementTimeMs = millis();
 
   if (error) {
     Serial.print("Error trying to execute readMeasurement(): ");
     errorToString(error, errorMessage, 256);
-    Serial.println(F(errorMessage));
+    Serial.println(errorMessage);
     return;
   }
 
   if (co2 == 0) {
-    Serial.println(F("Invalid sample detected, skipping."));
+    Serial.println("Invalid sample detected, skipping.");
     return;
   }
-
+  
+  Serial.print("co2:");
   Serial.print(co2);
   Serial.print("\t");
+  Serial.print("temperature:");
   Serial.print(temperature);
   Serial.print("\t");
-  Serial.println(F(humidity));
+  Serial.print("humidity:");
+  Serial.println(humidity);
 
-  gadgetBle.writeCO2(co2);
-  gadgetBle.writeTemperature(temperature);
-  gadgetBle.writeHumidity(humidity);
-  gadgetBle.commit();
-}
+  provider.writeValueToCurrentSample(co2, Unit::CO2);
+  provider.writeValueToCurrentSample(temperature, Unit::T);
+  provider.writeValueToCurrentSample(humidity, Unit::RH);
 
-void printLocalTime(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println(F("Failed to obtain time"));
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println((&timeinfo, "%Y"));
-  Serial.print("Hour: ");
-  Serial.println((&timeinfo, "%H"));
-  Serial.print("Hour (12 hour format): ");
-  Serial.println((&timeinfo, "%I"));
-  Serial.print("Minute: ");
-  Serial.println((&timeinfo, "%M"));
-  Serial.print("Second: ");
-  Serial.println((&timeinfo, "%S"));
-
-  Serial.println(F("Time variables"));
-  char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
-  Serial.println(F(timeHour));
-  char timeWeekDay[10];
-  strftime(timeWeekDay,10, "%A", &timeinfo);
-  Serial.println(F(timeWeekDay));
-  Serial.println(F());
+  provider.commitSample();
+  
 }
